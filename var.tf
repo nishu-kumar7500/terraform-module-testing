b9876927d6d2f57bf24b711912fe9162b416a7fd
Rajput7500 <132784995+milton7500@users.noreply.github.com>
2025-01-12 16:25:44 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,19 +1,24 @@
 variable "resource_group_name" {
   type = string
+  description = "resource group name"
 }
 
 variable "location" {
   type = string
+  description = "resource location"
 }
 
 variable "storage_account" {
   type = string
+  description = "storage account name as per ENV"
 }
 
 variable "account_replication_type" {
   type = string
+  description = "resource type for storage account"
 }
 
 variable "environment" {
   type = string
+  description = "resource ENV"
 }
