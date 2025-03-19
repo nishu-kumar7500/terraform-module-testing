77d68073b46c62a94d7c0944625c47f6f488defc
DevOps7500 <132784995+nishu-kumar7500@users.noreply.github.com>
2025-03-19 13:15:03 +0530

    Create main.tf

---
+++ Service/main.tf
@@ -0,0 +1,21 @@
+module "resource_group_budget" {
+  source = "../azure_budget_alert"
+
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  budget_name                   = var.budget_name
+  amount                        = var.amount
+  time_grain                    = var.time_grain
+  start_date                    = var.start_date
+  end_date                      = var.end_date
+  notification_enabled          = var.notification_enabled
+  notification_threshold_1      = var.notification_threshold_1
+  notification_operator_1       = var.notification_operator_1
+  notification_threshold_type_1 = var.notification_threshold_type_1
+  contact_emails_1              = var.contact_emails_1
+  contact_roles_1               = var.contact_roles_1
+  notification_enabled_2        = var.notification_enabled_2
+  notification_threshold_2      = var.notification_threshold_2
+  notification_operator_2       = var.notification_operator_2
+  contact_emails_2              = var.contact_emails_2
+}
