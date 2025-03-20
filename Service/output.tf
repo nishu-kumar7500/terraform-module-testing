7cec9597a9e9440a79ababa85a723a4d7526700d
DevOps7500 <132784995+nishu-kumar7500@users.noreply.github.com>
2025-03-20 13:10:50 +0530

    Update output.tf

--- Service/output.tf
+++ Service/output.tf
@@ -1,9 +1,4 @@
-output "resource_group_id" {
-  description = "The ID of the created resource group"
-  value       = module.resource_group_budget.resource_group_id
-}
-
 output "budget_id" {
   description = "The ID of the created budget"
-  value       = module.resource_group_budget.budget_id
+  value       = module.azure_budget.budget_id
 }
