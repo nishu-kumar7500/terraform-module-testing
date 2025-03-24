623d00dc99d553927aac82cc446016c59b848e13
DevOps7500 <132784995+nishu-kumar7500@users.noreply.github.com>
2025-03-24 17:32:10 +0530

    Update main.tf

--- Service/main.tf
+++ Service/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 3.54.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+
+
 module "azure_budget" {
   source = "../azure_budget_alert"
 
