ea395c4854e4e92205701fb3a5697782144fd407
DevOps7500 <132784995+nishu-kumar7500@users.noreply.github.com>
2025-03-25 15:16:39 +0530

    Update output.tf

--- Module/output.tf
+++ Module/output.tf
@@ -1,4 +1,4 @@
 output "budget_id" {
   description = "The ID of the created budget"
-  value       = azurerm_consumption_budget_resource_group.example.id
+  value       = azurerm_consumption_budget_resource_group.consumption_budget.id
 }
